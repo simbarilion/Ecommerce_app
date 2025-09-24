23dc1e216b721821396d37a1821c79e76f572f2a
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-09-25 02:37:04 +0700

    created superuser, objects for models in catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description",)
+    search_fields = ("name",)
+
+@admin.register(Product)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "price", "created_at", "updated_at",)
+    list_filter = ("category", "price", "created_at", "updated_at",)
+    search_fields = ("name", "description", "category__name", "price", "created_at", "updated_at",)
