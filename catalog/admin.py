3614e9cc09d4b5f9aed0d4a24676a9517392276e
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-09-26 00:03:39 +0700

    modified html files, README.md, admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Contacts
 
 
 @admin.register(Category)
@@ -12,3 +12,8 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "category", "price", "created_at", "updated_at",)
     list_filter = ("category", "price", "created_at", "updated_at",)
     search_fields = ("name", "description", "category__name", "price", "created_at", "updated_at",)
+
+@admin.register(Contacts)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "country", "address", "email",)
+    search_fields = ("id", "email",)
