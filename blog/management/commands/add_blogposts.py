53369fc489d395223923838e7b6e7468aed42ac7
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-10-23 23:08:10 +0700

    modified custom command for blog

--- blog/management/commands/add_blogposts.py
+++ blog/management/commands/add_blogposts.py
@@ -1,42 +1,19 @@
-import json
-import os
-
-from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
 
 from blog.models import Blogpost
 
 
-# class Command(BaseCommand):
-#     help = "Загружает тестовые данные из фикстур blog/fixtures/"
-#
-#     def handle(self, *args, **kwargs):
-#         Blogpost.objects.all().delete()
-#
-#         base_dir = os.path.join("blog", "fixtures")
-#         users_fixture = os.path.join(base_dir, "users_fixture.json")
-#         posts_fixture = os.path.join(base_dir, "blogposts_fixture.json")
-#
-#         try:
-#             with open(users_fixture, encoding="utf-8") as f:
-#                 users = json.load(f)
-#
-#             for u in users:
-#                 fields = u["fields"]
-#                 if not User.objects.filter(username=fields["username"]).exists():
-#                     User.objects.create_user(
-#                         username=fields["username"],
-#                         email=fields.get("email", ""),
-#                         first_name=fields.get("first_name", ""),
-#                         last_name=fields.get("last_name", ""),
-#                         password="pbkdf2_sha256$600000$dummy$fakehash"
-#                     )
-#             self.stdout.write(self.style.SUCCESS("Загружены пользователи из фикстуры"))
-#
-#             call_command("loaddata", posts_fixture)
-#             self.stdout.write(self.style.SUCCESS("Загружены статьи из фикстуры"))
-#         except Exception as e:
-#             raise CommandError(f"Ошибка при загрузке фикстур: {e}")
-#
-#         self.stdout.write(self.style.SUCCESS("Данные успешно загружены"))
+class Command(BaseCommand):
+    help = "Очищает таблицу статей пользователей и загружает данные из фикстуры blog/fixtures/blogposts_fixture.json"
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write(self.style.WARNING("Очистка таблицы статей пользователей..."))
+        Blogpost.objects.all().delete()
+
+        try:
+            call_command("loaddata", "blog/fixtures/blogposts_fixture.json")
+            self.stdout.write(self.style.SUCCESS("Данные успешно загружены"))
+
+        except Exception as e:
+            raise CommandError(f"Ошибка при загрузке фикстур: {e}")
