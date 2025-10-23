65ee3fb4218a81aec299686f0249d9969399e304
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-10-24 00:10:23 +0700

    created templates for app blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Blogpost
 
-# Register your models here.
+
+@admin.register(Blogpost)
+class BlogpostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "created_at",
+                    "updated_at", "status", "number_of_views",)
+    list_editable = ("status",)
+    list_filter = ("author", "created_at", "updated_at", "status", "number_of_views")
+    search_fields = ("id", "title",)
